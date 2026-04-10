7698a3d17952fac547f80acada9fe2fcaeff01f0
diegodevtech <diegodevtechnology@gmail.com>
2026-04-10 16:39:24 -0400

    config(middleware)
    -creates error-handling middleware using custom class AppError and starts calling it at server after all routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { routes } from "./routes";
+import { errorHandlingMiddleware } from "./middlewares/error-handling";
 
 const PORT = 3333;
 const app = express();
@@ -7,4 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+
+app.use(errorHandlingMiddleware);
 app.listen(PORT, () => console.log("Running on PORT: ", PORT))
