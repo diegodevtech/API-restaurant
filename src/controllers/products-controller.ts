b74f3c25c0977b43b48bdfe56b02e74983a8c4e5
diegodevtech <diegodevtechnology@gmail.com>
2026-04-13 14:11:02 -0400

    feat(getAll)
    -starts interacting with the query params and filtering using wherLike

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -5,7 +5,9 @@ import { z } from "zod";
 class ProductController {
   async getAllProducts(request: Request, response: Response, next: NextFunction) {
     try{
-      return response.json({ message: "OK" });
+      const { name } = request.query;
+      const products = await knex<ProductRepository>("products").select().whereLike("name", `%${name ?? ""}%`).orderBy("name");
+      return response.json(products);
     } catch (error) {
       // passes the error while continuing the middleware chain
       next(error);
