1a8e1963dc3a8f573448a1752095e052c59376a2
diegodevtech <diegodevtechnology@gmail.com>
2026-04-13 16:43:41 -0400

    feat(tables-sessions)
    -starts validation with zod for tables-sessions table

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,9 +1,23 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 import { knex } from "../database/knex";
+import { TablesSessionsRepository } from "@/database/types/tables-sessions-repository";
 
 class TablesSessionsController {
   async createSession(request: Request, response: Response, next: NextFunction) {
     try {
+
+      const bodySchema = z.object({
+        table_id: z.number()
+      });
+
+      const { table_id } = bodySchema.parse(request.body);
+
+      await knex<TablesSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
       return response.status(201).json()
     } catch (error) {
       next(error);
